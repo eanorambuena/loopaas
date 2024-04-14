725cc08a3a2c465c08de9d7502f5a115be814ded
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-04-14 11:08:14 -0400

    Add public folder

--- components/EvaluationCard.tsx
+++ components/EvaluationCard.tsx
@@ -1,13 +1,14 @@
 import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 import Badge from "@/components/Badge";
+import Image from "next/image";
 
 export default async function EvaluationCard({ evaluation, params }: { evaluation: any, params: any }) {
   const supabase = createClient();
 
   return (
     <Link className="w-full max-w-4xl rounded-md bg-foreground/5 hover:bg-foreground/10" href={`/cursos/`}>
-      <img src={"/background-color.webp"/* evaluation.img */} alt={evaluation.title} className="w-full rounded-t-md" />
+      <Image src={"/background-color.webp"/* evaluation.img */} alt={evaluation.title} className="w-full rounded-t-md" width={500} height={500} />
       <div className="flex gap-2 p-4 flex-col">
         <div className="flex gap-2 justify-between">
           <p className="text-sm font-normal text-gray-400">{params.abbreviature}</p>
