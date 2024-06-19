62b9e6a8956ac79ce4f049dcfb51c7932c494492
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-06-19 18:18:58 -0400

    fix: No use found case

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -9,11 +9,15 @@ export default async function AuthButton() {
     data: { user },
   } = await supabase.auth.getUser()
 
-  const { data: userInfo } = await supabase
-    .from("userInfo")
-    .select("*")
-    .eq("userId", user.id)
-    .single()
+  let name;
+  if (user) {
+    const { data: userInfo } = await supabase
+      .from("userInfo")
+      .select("*")
+      .eq("userId", user.id)
+      .single()
+    name = userInfo.firstName
+  }
 
   const signOut = async () => {
     "use server"
@@ -25,7 +29,9 @@ export default async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4">
-      <span className="hidden sm:inline">Hola, {userInfo.firstName}!</span>
+      <span className="hidden sm:inline">
+        Hola, {name}!
+      </span>
       <form action={signOut}>
         <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
           Cerrar sesión
