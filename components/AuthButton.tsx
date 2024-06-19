5e6802e7201cc9ca95e3870e02a72cc003de86ad
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-06-19 18:14:02 -0400

    feat: Hide greeting in AuthButton.tsx for small devices

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -9,6 +9,12 @@ export default async function AuthButton() {
     data: { user },
   } = await supabase.auth.getUser()
 
+  const { data: userInfo } = await supabase
+    .from("userInfo")
+    .select("*")
+    .eq("userId", user.id)
+    .single()
+
   const signOut = async () => {
     "use server"
 
@@ -19,7 +25,7 @@ export default async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hola, {user.email}!
+      <span className="hidden sm:inline">Hola, {userInfo.firstName}!</span>
       <form action={signOut}>
         <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
           Cerrar sesión
