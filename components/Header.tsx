89252f3d00170caa7845aee4217f687f0fedff97
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-06-19 20:30:05 +0000

    feat: Update Header component to handle Supabase authentication and display user information

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,8 @@
+import { Link } from "lucide-react"
 import AuthButton from "../components/AuthButton"
 import { createClient } from "@/utils/supabase/server"
 
-export default function Header() {
+export default async function Header() {
   const canInitSupabaseClient = () => {
     // This function is just for the interactive tutorial.
     // Feel free to remove it once you have Supabase connected.
@@ -14,11 +15,42 @@ export default function Header() {
   }
 
   const isSupabaseConnected = canInitSupabaseClient()
+  if (!isSupabaseConnected) return (
+    <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+      <div className="w-full max-w-4xl flex justify-end items-center p-3 text-sm" />
+    </nav>
+  )
+
+  const supabase = createClient()
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  const noUserOrUserInfoFallback = (
+    <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+      <div className="w-full max-w-4xl flex justify-end items-center p-3 text-sm">
+        <AuthButton />
+      </div>
+    </nav>
+  )
+
+  if (!user) return noUserOrUserInfoFallback
+
+  const { data: userInfo } = await supabase
+    .from("userInfo")
+    .select("*")
+    .eq("userId", user.id)
+    .single()
+
+  if (!userInfo) return noUserOrUserInfoFallback
 
   return (
     <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+      <Link size="24" className="ml-2" href="/perfil" />
       <div className="w-full max-w-4xl flex justify-end items-center p-3 text-sm">
-        {isSupabaseConnected && <AuthButton />}
+        <p>Bienvenido {userInfo.firstName}</p>
+        <AuthButton />
       </div>
     </nav>
   )
