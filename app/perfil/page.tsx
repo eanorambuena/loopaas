361892a3914fb93ee082a9b21989b672ca87094a
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-06-19 20:57:26 +0000

    feat: Update Page component to include personal information section

--- app/perfil/page.tsx
+++ app/perfil/page.tsx
@@ -2,8 +2,14 @@ import WannabeProfessorForm from "./WannabeProfessorForm"
 
 export default function Page() {
   return (
-    <div className="flex-1 flex flex-col w-full sm:max-w-lg px-8 justify-center gap-6">
+    <div className="animate-in flex-1 flex flex-col w-full sm:max-w-lg px-8 justify-center gap-6">
       <h1 className="text-3xl font-bold">Perfil</h1>
+      <section className="flex flex-col gap-4">
+        <h2 className="text-xl font-bold">Información Personal</h2>
+        <p className="text-foreground">
+          Actualiza tu información personal
+        </p>
+      </section>
       <WannabeProfessorForm />
     </div>
   )
