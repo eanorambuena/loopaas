bdfe32a0eeec65d92cebc7e201fc40b48dbfa709
Emmanuel Norambuena <eanorambuena@uc.cl>
2025-06-16 18:42:35 +0000

    fix: Importar NextResponse en middleware para mejorar la gestión de respuestas

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { updateSession } from '@/utils/supabase/middleware'
 import { auth0 } from '@/lib/auth0'
 
