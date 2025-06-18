19c53514f28f79d1b7e551eaa7ca4dd8fa47463a
Emmanuel Norambuena <eanorambuena@uc.cl>
2025-06-18 15:04:25 +0000

    feat: Agregar campo de contraseña en el archivo de configuración y mejorar formato de fecha en el formulario

--- app/cursos/[abbreviature]/[semester]/evaluaciones/[id]/configuracion/ConfigForm.tsx
+++ app/cursos/[abbreviature]/[semester]/evaluaciones/[id]/configuracion/ConfigForm.tsx
@@ -115,7 +115,14 @@ export default function ConfigForm({ evaluation }: Props) {
     <form className='animate-in flex-1 flex flex-col w-full justify-center items-center gap-6 text-foreground' onSubmit={handleSubmit}>
       <Input label='Título' name='title' required defaultValue={evaluation.title} className='w-full' />
       <Input type='textarea' label='Instrucciones' name='instructions' required defaultValue={evaluation.instructions} className='w-full' />
-      <Input type='date' label='Fecha de entrega' name='deadLine'required defaultValue={evaluation.deadLine} className='w-full' />
+      <Input
+        type='date'
+        label='Fecha de entrega'
+        name='deadLine'
+        required
+        defaultValue={evaluation.deadLine}
+        className='w-full'
+      />
       { questions
         ? Object.entries(questions).map(([id, question]) => (
           <QuestionForm id={id} question={question} key={id} deleteQuestion={deleteQuestion} />
