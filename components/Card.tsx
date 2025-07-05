75efb2c5d284931b9f449104c8554ea72e6300b2
Emmanuel Norambuena <eanorambuena@uc.cl>
2025-07-05 04:27:29 -0400

    style: update Card component to improve responsiveness by adjusting max-width in CardBody for better layout handling

--- components/Card.tsx
+++ components/Card.tsx
@@ -13,7 +13,7 @@ export default function Card({ icon, path, title, className } : CardProps) {
   if (!path) {
     return (
       <CardContainer>
-        <CardBody className={`size-16 bg-gray-50 aspect-square flex flex-col justify-center items-center relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border ${className}`}>
+        <CardBody className={`max-w-4xl bg-gray-50 aspect-square flex flex-col justify-center items-center relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border ${className}`}>
           <CardItem className='flex flex-col justify-center items-center'>
             {icon({ size: 48 })}
             <h3 className="text-xl font-bold">{title}</h3>
