df3bdb391f622bb2541ec0696057c3b5f07e1860
Emmanuel Norambuena <eanorambuena@uc.cl>
2025-07-05 03:50:58 -0400

    refactor: simplify Card component structure by replacing internal layout with a self-referential Card call for improved maintainability

--- components/Card.tsx
+++ components/Card.tsx
@@ -28,10 +28,7 @@ export default function Card({ icon, path, title, className } : CardProps) {
       className={`w-full max-w-4xl rounded-md flex justify-center items-center bg-[#eeeeee] dark:bg-gray-900 hover:scale-105 transition-transform duration-300 hover:shadow-[0_20px_30px_rgba(_8,_184,_112,_0.7)] ${className}`}
       href={path}
     >
-      <div className="flex gap-2 p-10 flex-col items-center justify-center">
-        {icon({ size: 48 })}
-        <h3 className="text-xl font-bold">{title}</h3>
-      </div>
+      <Card icon={icon} title={title} className={className} />
     </Link>
   )
 }
