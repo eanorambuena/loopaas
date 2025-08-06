8206c58babfa3913789cbd2be14ac024bec2f083
Emmanuel Norambuena <eanorambuena@uc.cl>
2025-08-06 20:18:46 +0000

    feat: Añadir componente LinkPreview para vista previa de enlaces con soporte de HoverCard

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ const withNextIntl = require('next-intl/plugin')('./i18n.ts')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [
+      'api.microlink.io', // Microlink Image Preview
+    ],
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint warnings.
