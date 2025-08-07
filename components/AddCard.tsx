f71eed6f5532fe2d9a236113e8a55573ce59e831
Emmanuel Norambuena <eanorambuena@uc.cl>
2025-08-07 12:30:34 +0000

    feat: Refactor AddCard component to use CardContainer and add link support

--- components/AddCard.tsx
+++ components/AddCard.tsx
@@ -1,13 +1,32 @@
-import Card, { CardProps } from '@/components/Card'
+import { CardContainer, CardBody, CardItem } from '@/components/ui/3d-card'
 import PlusIcon from '@/components/icons/PlusIcon'
+import Link from 'next/link'
 
-interface AddCardProps extends Omit<CardProps, 'icon'> {}
+interface AddCardProps {
+  title?: string
+  path?: string
+  className?: string
+}
 
-export function AddCard(props: AddCardProps) {
-  return (
-    <Card
-      icon={PlusIcon}
-      {...props}
-    />
+export function AddCard({ title, path, className }: AddCardProps) {
+  const content = (
+    <CardContainer containerClassName="p-0 w-fit h-fit">
+      <CardBody className={`size-64 bg-gray-50 dark:bg-black dark:border-white/[0.2] border-black/[0.1] rounded-xl border flex flex-col gap-4 justify-center items-center p-6 ${className}`}>
+        <CardItem className='flex flex-col justify-center items-center' translateZ="20">
+          <PlusIcon size={48} />
+          {title && <h3 className="text-xl font-bold text-center">{title}</h3>}
+        </CardItem>
+      </CardBody>
+    </CardContainer>
   )
+
+  if (path) {
+    return (
+      <Link href={path}>
+        {content}
+      </Link>
+    )
+  }
+
+  return content
 }
